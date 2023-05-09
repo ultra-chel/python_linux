bf2c84761145d2e13517fd73736553bd0891e49d
ubPK <ubpk@ubPK.virtualbox>
2023-05-09 19:33:18 +0700

    окончательная доработка парсера

--- parser.py
+++ parser.py
@@ -1,68 +1,74 @@
 import argparse
 import re
-import json
 import os
+import json
 from collections import defaultdict
 from os import listdir
 from os.path import join, isfile
-from operator import itemgetter
 
 
 def main(file):
-    dict_ip = defaultdict(
-        lambda: {"GET": 0, "POST": 0, "PUT": 0, "DELETE": 0, "HEAD": 0}
-    )
+    dict_ip = {"TOTAL": 0, "METHOD": {"GET": 0, "POST": 0, "PUT": 0, "DELETE": 0, "HEAD": 0, "OPTIONS": 0}}
+    dict_ip_requests = defaultdict(lambda: {"REQUESTS_COUNT": 0})
+    list_ip_duration = []
 
-    ips = []
+    with open(file) as logfile:
 
-    with open(file) as f:
-        idx = 0
-        for line in f:
-            if idx > 99:
-                break
+        for line in logfile:
+            method = re.search(r"\] \"(POST|GET|PUT|DELETE|HEAD|OPTIONS)", line)
+            ip = re.search(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", line).group()
+            duration = int(line.split()[-1])
+            date = re.search(r"\[\d.*?\]", line)
+            url = re.search(r"\"http.*?\"", line)
 
-            # 109.169.248.247 - - [12/Dec/2015:18:25:11 +0100] "GET /administrator/ HTTP/1.1" 200 4263
-            # "-" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" 7269
-            ip_match = re.search(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", line)
-            if ip_match is not None:
-                ip = ip_match.group()
-                ips.append(ip)
-                method = re.search(r"\] \"(POST|GET|PUT|DELETE|HEAD)", line)
-                if method is not None:
-                    dict_ip[ip][method.group(1)] += 1
-                    idx += 1
-    print(ips)
-    unique_ips = set(ips)
-    print(unique_ips)
-    # for ip in unique_ips:
-    result = {i: ips.count(i) for i in ips}
-    print(result)
-    sorted_result = dict(sorted(result.items(), key=itemgetter(1), reverse=True))
-    print(sorted_result)
-    for key, values in sorted_result.items():
-        i = 0
-        while i < 3:
-            top_ips = {key: values}
-            i += 1
-    #print(top_ips)
-    #dict.items(1)
+            dict_ip["TOTAL"] += 1
 
-    print(sorted_result.items())
-    print(next(iter(sorted_result)))
-    print(json.dumps(dict_ip, indent=4))
+            if method is not None:
+
+                dict_ip["METHOD"][method.group(1)] += 1
+                dict_ip_requests[ip]["REQUESTS_COUNT"] += 1
+
+                dict_t = {"ip": ip,
+                          "date": date.group(0).split(" ")[0].lstrip("["),
+                          "method": method.group(1),
+                          "url": "-",
+                          "duration": duration
+                          }
+
+                if url is not None:
+                    dict_t["url"] = url.group(0).strip("\"")
+
+                list_ip_duration.append(dict_t)
+
+        top_ips = dict(sorted(dict_ip_requests.items(), key=lambda x: x[1]["REQUESTS_COUNT"], reverse=True)[0:3])
+        top_slw_req = sorted(list_ip_duration, key=lambda x: x["duration"], reverse=True)[0:3]
+
+        result = {"top_ips": top_ips,
+                  "top_longest": top_slw_req,
+                  "total_stat": dict_ip["METHOD"],
+                  "total_requests": dict_ip["TOTAL"],
+                  }
+
+        with open(f"{file}.json", "w", encoding="utf-8") as f:
+            result = json.dumps(result, indent=4)
+            f.write(result)
+            print(f"\n===== RESULT STATS ON FILE: {file} =====\n {result}")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", dest="path", help="Path to logfile")
+    parser.add_argument("-p", dest="path", help="Path to logfile")
     args = parser.parse_args()
 
     log_files = []
     if isfile(args.path):
         log_files.append(args.path)
-    else:
+    elif os.path.isdir(args.path):
         for file in listdir(args.path):
-            log_files.append(join(args.path, file))
+            if file.endswith(".log"):
+                log_files.append(join(args.path, file))
+    else:
+        print("ERROR: Incorrect path to log file or directory")
 
     for f in log_files:
         print(f'Processing file {f}...')
